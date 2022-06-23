667f748175b2d3965926d75f62591275083d7c19
Victor Torres <vikthort.t@gmail.com>
2022-06-23 11:21:31 -0500

    pet project

---
+++ src/components/ColorButton.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    color: {
+      type: String,
+      default: 'initial'
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<template>
+  <button>{{text}}</button>
+</template>
+
+<style scoped>
+  button {
+    color: v-bind(color)
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import HelloWorld from './components/HelloWorld.vue'
+import ColorButton from './components/ColorButton.vue'
+export { ColorButton, HelloWorld }
+
+export * from './utils/sum'
